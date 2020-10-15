1e9374df1c3d31a31421763e2ed4dfcb8dc01e2c
Peter Kofler <peter.kofler@code-cop.org>
2020-10-15 22:14:30 +0200

    Document types.

--- code.js
+++ code.js
@@ -1,8 +1,28 @@
+/**
+ * Get the 2D context of the given element.
+ * @param {string} elementId
+ * @returns {CanvasRenderingContext2D}
+ */
 function getGraphicsFor(elementId) {
     var c = document.getElementById(elementId);
     return c.getContext("2d");
 }
 
+/**
+ * Represents a point in the 2D game area. Sub domain Math.
+ * @param {number} x
+ * @param {number} y
+ */
+function Point(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+/**
+ * Represents the dimension of the 2D game area. Sub domain Math.
+ * @param {number} width
+ * @param {number} height
+ */
 function Dimension(width, height) {
     this.width = width;
     this.height = height;
@@ -23,43 +43,65 @@ function Dimension(width, height) {
     };
 }
 
-function Point(x, y) {
-    this.x = x;
-    this.y = y;
-}
-
+/**
+ * Represents a grid of pixels in given dimension. Sub domain Computer Graphics.
+ * @param {CanvasRenderingContext2D} graphics
+ * @param {Dimension} dimension
+ */
 function PixelGrid(graphics, dimension) {
     function clear() {
         graphics.fillRect(0, 0, dimension.width, dimension.height);
     }
 
+    /**
+     * @param {Point} point
+     */
     this.putPixel = function(point) {
         graphics.clearRect(point.x, point.y, 1, 1, 0);
     };
 
     clear();
 }
-
+/**
+ * Structure to mark positions in 2D space.
+ * @param {number} maxX
+ */
 function Marker2D(maxX) {
     var occupied = [];
 
+    /**
+     * @param {Point} point
+    */
     function toIndex(point) {
         return maxX * point.y + point.x;
     }
 
+    /**
+     * @param {Point} point
+     */
     this.isFree = function(point) {
         var index = toIndex(point);
         return occupied[index] === undefined
     };
 
+    /**
+     * @param {Point} point
+     */
     this.mark = function(point) {
         var index = toIndex(point);
         occupied[index] = 1;
     };
 }
 
+/**
+ * The game of Tron.
+ * @param {PixelGrid} grid
+ * @param {Marker2D} trail
+ * @param {Dimension} dimension
+ * @param {number} speed
+ */
 function Game(grid, trail, dimension, speed) {
-    var p = dimension.center();
+    var position = dimension.center();
     var score = 0;
 
     this.lastKeyEvent = 0;
@@ -75,14 +117,14 @@ function Game(grid, trail, dimension, speed) {
 
     this.advance = function() {
         if (this.lastKeyEvent) {
-            var hitsWall = dimension.isOutside(p);
+            var hitsWall = dimension.isOutside(position);
             if (hitsWall) {
                 this.collision();
                 return
             }
 
-            var x = p.x;
-            var y = p.y;
+            var x = position.x;
+            var y = position.y;
             switch (this.lastKeyEvent.which & 3) {
                 case 0:
                     x += 1;
@@ -97,9 +139,9 @@ function Game(grid, trail, dimension, speed) {
                     y += 1;
                     break;
             }
-            p = new Point(x, y);
+            position = new Point(x, y);
 
-            if (trail.isFree(p)) {
+            if (trail.isFree(position)) {
                 this.tronMoves();
             } else {
                 this.collision();
@@ -108,8 +150,8 @@ function Game(grid, trail, dimension, speed) {
     };
 
     this.tronMoves = function() {
-        grid.putPixel(p);
-        trail.mark(p);
+        grid.putPixel(position);
+        trail.mark(position);
         score = score + 1;
     };
 
@@ -125,8 +167,7 @@ var game;
 
 function startGame() {
     var graphics = getGraphicsFor("gc");
-    var size = 150;
-    var dimension = new Dimension(size, size);
+    var dimension = new Dimension(150, 150);
     var grid = new PixelGrid(graphics, dimension);
     var trail = new Marker2D(dimension.width);
     game = new Game(grid, trail, dimension, 9);
