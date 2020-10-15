381799f45813add6a0ea6270273c6ee7c20931de
Peter Kofler <peter.kofler@code-cop.org>
2020-10-15 14:11:10 +0200

    Expand wall collisions.

--- code.js
+++ code.js
@@ -5,8 +5,8 @@ var graphics;
 var grid = {};
 var score = 0;
 var width = 150;
-var x;
-var i;
+var tronPos;
+var threadHandle;
 
 function keyPressed(event) {
     lastKeyEvent = event;
@@ -16,29 +16,54 @@ function startGame() {
     graphics = c.getContext('2d');
     grid = {};
     score = 0;
-    x = width * 75 + 75; // Tron's position, and is set to 11325 (11325=150*75+75=center of the grid).
-    graphics.fillRect(0, 0, width, x);
-    i = setInterval(interval, 9);
+    tronPos = width * (width/2) + (width/2); // Tron's position, and is set to 11325 (11325=150*75+75=center of the grid).
+    graphics.fillRect(0, 0, width, tronPos);
+    threadHandle = setInterval(advanceGame, 9);
 }
 
-function interval() {
+function advanceGame() {
     if (lastKeyEvent) {
-        if (0 < x % width &&
-            x < width * width &&
-            (grid[x += [1, -width, -1, width][lastKeyEvent.which & 3]] ^= 1)) { // Updates x based on the value of e (remember, onkeyup saves the event in the e variable).
+        var x = tronPos % width;
+        var y = tronPos / width;
+        if (x <= 0) {
+            // left wall (and right wall)
+            collision();
+            return
+        }
+        if (x < 0) {
+            // upper wall
+            collision();
+            return
+        }
+        if (y >= width) {
+            // lower wall
+            collision();
+            return
+        }
+
+        tronPos += [1, -width, -1, width][lastKeyEvent.which & 3];
+        if ((grid[tronPos] ^= 1)) {
+            // Updates x based on the value of e .
+
+            var x = tronPos % width;
+            var y = tronPos / width;
 
             // draw a white pixel at the Tron's position
-            graphics.clearRect(x % width, x / width, 1, 1, score++);
+            graphics.clearRect(x, y, 1, 1, score++);
 
-        } else {
-
-            // We replace the page's content with "game over" and the score when a collision occurs.
-            document.body.innerHTML = 'game over: ' + score;
-            clearInterval(i);
+            return;
         }
-    }
 
+        collision();
+    }
+}
+
+function collision() {
+    // We replace the page's content with "game over" and the score.
+    document.body.innerHTML = 'game over: ' + score;
+    clearInterval(threadHandle);
 }
 
 window.onload = startGame;
 window.onkeydown = keyPressed;
+
