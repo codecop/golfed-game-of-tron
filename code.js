8aa39f4ad6a60e8e9ca622bbb69198acceab4681
Peter Kofler <peter.kofler@code-cop.org>
2020-10-15 21:37:22 +0200

    Move everything into classes.

--- code.js
+++ code.js
@@ -1,20 +1,9 @@
-var lastKeyEvent = 0;
-var ui;
-var trail;
-var score = 0;
-var width = 150;
-var tronX = 0;
-var tronY = 0;
-var threadHandle;
-
-function keyPressed(event) {
-    lastKeyEvent = event;
+function getGraphics() {
+    var c = document.getElementById("gc");
+    return c.getContext('2d');
 }
 
-function DrawingArea() {
-    var c = document.getElementById("gc");
-    var graphics = c.getContext('2d');
-
+function DrawingGrid(graphics) {
     this.clear = function(width, height) {
         graphics.fillRect(0, 0, width, height);
     };
@@ -25,81 +14,100 @@ function DrawingArea() {
 }
 
 function TronTrail(width) {
-    var tronTrail = [];
+    var trail = [];
 
-    function asPos(x, y) {
+    function toIndex(x, y) {
         return width * y + x;
     }
 
     this.isFree = function(x, y) {
-        var tronPos = asPos(x, y);
-        return tronTrail[tronPos] === undefined
+        var index = toIndex(x, y);
+        return trail[index] === undefined
     };
 
     this.mark = function(x, y) {
-        var tronPos = asPos(x, y);
-        tronTrail[tronPos] = 1;
+        var index = toIndex(x, y);
+        trail[index] = 1;
     };
 }
 
-function startGame() {
-    trail = new TronTrail(width);
+function Game(ui, trail, width) {
 
-    score = 0;
-    tronX = width / 2;
-    tronY = width / 2;
-
-    ui = new DrawingArea();
+    this.lastKeyEvent = 0;
+    var score = 0;
+    var tronX = width / 2;
+    var tronY = width / 2;
     ui.clear(width, width);
+    var threadHandle;
 
-    threadHandle = setInterval(advanceGame, 9);
+    this.start = function() {
+        var self = this;
+        function advance() {
+            self.advance();
+        }
+        threadHandle = setInterval(advance, 9);
+    };
+
+    this.advance = function() {
+        if (this.lastKeyEvent) {
+            var hitsWall = (tronX <= 0) || (tronX >= width) || (tronY < 0) || (tronY >= width);;
+            if (hitsWall) {
+                this.collision();
+                return
+            }
+
+            switch (this.lastKeyEvent.which & 3) {
+                case 0:
+                    tronX += 1;
+                    break;
+                case 1:
+                    tronY -= 1;
+                    break;
+                case 2:
+                    tronX -= 1;
+                    break;
+                case 3:
+                    tronY += 1;
+                    break;
+            }
+
+            if (trail.isFree(tronX, tronY)) {
+                this.tronMoves();
+            } else {
+                this.collision();
+            }
+        }
+    };
+
+    this.tronMoves = function() {
+        ui.putPixel(tronX, tronY);
+        trail.mark(tronX, tronY);
+        score = score + 1;
+    };
+
+    this.collision = function() {
+        // We replace the page's content with "game over" and the score.
+        document.body.innerHTML = 'game over: ' + score;
+        clearInterval(threadHandle);
+    };
+
 }
 
-function advanceGame() {
-    if (lastKeyEvent) {
-        var hitsWall = (tronX <= 0) || (tronX >= width) || (tronY < 0) || (tronY >= width);;
-        if (hitsWall) {
-            collision();
-            return
-        }
+var game;
 
-        switch (lastKeyEvent.which & 3) {
-            case 0:
-                tronX += 1;
-                break;
-            case 1:
-                tronY -= 1;
-                break;
-            case 2:
-                tronX -= 1;
-                break;
-            case 3:
-                tronY += 1;
-                break;
-        }
+function startGame() {
+    var width = 150;
+    var ui = new DrawingGrid(getGraphics());
+    var trail = new TronTrail(width);;
+    game = new Game(ui, trail, width);
+    game.start();
+}
 
-        if (trail.isFree(tronX, tronY)) {
-
-            tronMoves();
-
-        } else {
-            collision();
-        }
+function keyPressed(event) {
+    if (game) {
+        game.lastKeyEvent = event;
     }
-}
-
-function tronMoves() {
-    ui.putPixel(tronX, tronY);
-    trail.mark(tronX, tronY);
-    score = score + 1;
-}
-
-function collision() {
-    // We replace the page's content with "game over" and the score.
-    document.body.innerHTML = 'game over: ' + score;
-    clearInterval(threadHandle);
 }
 
 window.onload = startGame;
 window.onkeydown = keyPressed;
-
