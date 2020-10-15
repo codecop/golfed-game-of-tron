8c1c106698252a2619059849a5f439593ef1fa1a
Peter Kofler <peter.kofler@code-cop.org>
2020-10-15 21:52:25 +0200

    Renames.

--- code.js
+++ code.js
@@ -1,56 +1,57 @@
-function getGraphics() {
-    var c = document.getElementById("gc");
-    return c.getContext('2d');
+function getGraphicsFor(elementId) {
+    var c = document.getElementById(elementId);
+    return c.getContext("2d");
 }
 
-function DrawingGrid(graphics) {
-    this.clear = function(width, height) {
+function PixelGrid(graphics, width, height) {
+    function clear() {
         graphics.fillRect(0, 0, width, height);
-    };
+    }
 
     this.putPixel = function(x, y) {
         graphics.clearRect(x, y, 1, 1, 0);
     };
+
+    clear();
 }
 
-function TronTrail(width) {
-    var trail = [];
+function Marker2D(maxX) {
+    var occupied = [];
 
     function toIndex(x, y) {
-        return width * y + x;
+        return maxX * y + x;
     }
 
     this.isFree = function(x, y) {
         var index = toIndex(x, y);
-        return trail[index] === undefined
+        return occupied[index] === undefined
     };
 
     this.mark = function(x, y) {
         var index = toIndex(x, y);
-        trail[index] = 1;
+        occupied[index] = 1;
     };
 }
 
-function Game(ui, trail, width) {
+function Game(grid, trail, width, height, speed) {
+    var x = width / 2;
+    var y = height / 2;
+    var score = 0;
 
     this.lastKeyEvent = 0;
-    var score = 0;
-    var tronX = width / 2;
-    var tronY = width / 2;
-    ui.clear(width, width);
-    var threadHandle;
 
-    this.start = function() {
+    var threadHandle;
+    this.startGameLoop = function() {
         var self = this;
         function advance() {
             self.advance();
         }
-        threadHandle = setInterval(advance, 9);
+        threadHandle = setInterval(advance, speed);
     };
 
     this.advance = function() {
         if (this.lastKeyEvent) {
-            var hitsWall = (tronX <= 0) || (tronX >= width) || (tronY < 0) || (tronY >= width);;
+            var hitsWall = (x <= 0) || (x >= width) || (y < 0) || (y >= height);
             if (hitsWall) {
                 this.collision();
                 return
@@ -58,20 +59,20 @@ function Game(ui, trail, width) {
 
             switch (this.lastKeyEvent.which & 3) {
                 case 0:
-                    tronX += 1;
+                    x += 1;
                     break;
                 case 1:
-                    tronY -= 1;
+                    y -= 1;
                     break;
                 case 2:
-                    tronX -= 1;
+                    x -= 1;
                     break;
                 case 3:
-                    tronY += 1;
+                    y += 1;
                     break;
             }
 
-            if (trail.isFree(tronX, tronY)) {
+            if (trail.isFree(x, y)) {
                 this.tronMoves();
             } else {
                 this.collision();
@@ -80,14 +81,14 @@ function Game(ui, trail, width) {
     };
 
     this.tronMoves = function() {
-        ui.putPixel(tronX, tronY);
-        trail.mark(tronX, tronY);
+        grid.putPixel(x, y);
+        trail.mark(x, y);
         score = score + 1;
     };
 
     this.collision = function() {
         // We replace the page's content with "game over" and the score.
-        document.body.innerHTML = 'game over: ' + score;
+        document.body.innerHTML = "game over: " + score;
         clearInterval(threadHandle);
     };
 
@@ -96,11 +97,12 @@ function Game(ui, trail, width) {
 var game;
 
 function startGame() {
-    var width = 150;
-    var ui = new DrawingGrid(getGraphics());
-    var trail = new TronTrail(width);;
-    game = new Game(ui, trail, width);
-    game.start();
+    var graphics = getGraphicsFor("gc");
+    var size = 150;
+    var grid = new PixelGrid(graphics, size, size);
+    var trail = new Marker2D(size);
+    game = new Game(grid, trail, size, size, 9);
+    game.startGameLoop();
 }
 
 function keyPressed(event) {
