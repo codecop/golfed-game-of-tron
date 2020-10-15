fe4e0b84dc7740fc9468eaf04e0931633fc64fb0
Peter Kofler <peter.kofler@code-cop.org>
2020-10-15 13:44:35 +0200

    Change comments. Start kata.

--- code.js
+++ code.js
@@ -10,7 +10,7 @@ function ol() {
     g = {} // We are going to use g to store our grid to detect collisions.
     s = 0 // keeps track of the score and is set to 0.
     n = 150
-    x = 11325 // tron's position, and is set to 11325 (11325=150*75+75=center of the grid).
+    x = 11325 // Tron's position, and is set to 11325 (11325=150*75+75=center of the grid).
     z.fillRect(0, 0, n, x);
     i = setInterval(interval, 9)
 }
@@ -21,7 +21,7 @@ function interval() {
             x < n * n &&
             (g[x += [1, -n, -1, n][e.which & 3]] ^= 1)) { // Updates x based on the value of e (remember, onkeyup saves the event in the e variable).
 
-            // draw a white pixel at the tron's position
+            // draw a white pixel at the Tron's position
             z.clearRect(x % n, x / n, 1, 1, s++)
 
         } else {
