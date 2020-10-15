44ccb6d199f0c8264ae532eed9ef615c463a5ffd
Peter Kofler <peter.kofler@code-cop.org>
2020-10-15 13:52:30 +0200

    Declare globals, rename stuff.

--- code.js
+++ code.js
@@ -1,38 +1,44 @@
-var e = 0 // e is going to contain the keyboard event
-var c = document.getElementById("gc")
+var lastKeyEvent = 0;
+var c = document.getElementById("gc");
 
-function ok(event) {
-    e = event
+var graphics;
+var grid = {};
+var score = 0;
+var width = 150;
+var x;
+var i;
+
+function keyPressed(event) {
+    lastKeyEvent = event;
 }
 
-function ol() {
-    z = c.getContext('2d');
-    g = {} // We are going to use g to store our grid to detect collisions.
-    s = 0 // keeps track of the score and is set to 0.
-    n = 150
-    x = 11325 // Tron's position, and is set to 11325 (11325=150*75+75=center of the grid).
-    z.fillRect(0, 0, n, x);
-    i = setInterval(interval, 9)
+function startGame() {
+    graphics = c.getContext('2d');
+    grid = {};
+    score = 0;
+    x = width * 75 + 75; // Tron's position, and is set to 11325 (11325=150*75+75=center of the grid).
+    graphics.fillRect(0, 0, width, x);
+    i = setInterval(interval, 9);
 }
 
 function interval() {
-    if (e) {
-        if (0 < x % n &&
-            x < n * n &&
-            (g[x += [1, -n, -1, n][e.which & 3]] ^= 1)) { // Updates x based on the value of e (remember, onkeyup saves the event in the e variable).
+    if (lastKeyEvent) {
+        if (0 < x % width &&
+            x < width * width &&
+            (grid[x += [1, -width, -1, width][lastKeyEvent.which & 3]] ^= 1)) { // Updates x based on the value of e (remember, onkeyup saves the event in the e variable).
 
             // draw a white pixel at the Tron's position
-            z.clearRect(x % n, x / n, 1, 1, s++)
+            graphics.clearRect(x % width, x / width, 1, 1, score++);
 
         } else {
 
             // We replace the page's content with "game over" and the score when a collision occurs.
-            document.body.innerHTML = 'game over: ' + s
-            clearInterval(i)
+            document.body.innerHTML = 'game over: ' + score;
+            clearInterval(i);
         }
     }
 
 }
 
-window.onload = ol
-window.onkeydown = ok
+window.onload = startGame;
+window.onkeydown = keyPressed;
